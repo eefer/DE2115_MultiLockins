c159c7049fd1d159b69f7beebfad6913c7d4cc71
arya1080 <arya.chowdhury.mugdha@gmail.com>
2017-10-24 12:39:14 -0600

    some minor modifications in tcl files

--- EightLockins/lockin_freq_scan.tcl
+++ EightLockins/lockin_freq_scan.tcl
@@ -1,9 +1,19 @@
+set masters [get_service_paths master]
+
+set master [lindex $masters 0]
+
+open_service master $master
+
+master_write_32 $master 0x20000000 8
+
 set filename "test.txt"
 set fileId [open $filename "w"]
-for {set i 12163} {$i < 16357} {incr i} {master_write_32 $master 0x00000000 $i
+for {set i 12163} {$i < 12500} {incr i} {master_write_32 $master 0x00000000 $i
 after 10
 set data [master_read_32 $master 0x30000000 1]
 set data2 [format %X $data]
 puts $fileId $data2
 }
 close $fileId
+
+close_service master $master
