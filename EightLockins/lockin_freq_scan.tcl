72550a5381d2369f1d3c719589633482dfbdd10e
arya1080 <arya.chowdhury.mugdha@gmail.com>
2017-10-27 15:21:56 -0600

    Some minor changes

--- EightLockins/lockin_freq_scan.tcl
+++ EightLockins/lockin_freq_scan.tcl
@@ -8,9 +8,9 @@ master_write_32 $master 0x20000000 8
 
 set filename "test.txt"
 set fileId [open $filename "w"]
-for {set i 12163} {$i < 12500} {incr i} {master_write_32 $master 0x00000000 $i
+for {set i 17481} {$i < 25669} {incr i} {master_write_32 $master 0x00000000 $i
 after 10
-set data [master_read_32 $master 0x30000000 1]
+set data [master_read_32 $master 0x31000000 1]
 set data2 [format %X $data]
 puts $fileId $data2
 }
