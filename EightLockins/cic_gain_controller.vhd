9afa5f56ac61de990d3f3fee028605d5c99656ab
arya1080 <arya.chowdhury.mugdha@gmail.com>
2018-01-19 11:44:16 -0700

    some minor changes in the cic filter

--- EightLockins/cic_gain_controller.vhd
+++ EightLockins/cic_gain_controller.vhd
@@ -11,7 +11,7 @@ USE ieee.numeric_std.all;
 entity cic_gain_controller is
 port(
   clk_in			: in  std_logic;		
-  data_in: in std_logic_vector(58 downto 0);
+  data_in: in std_logic_vector(57 downto 0);
   gain     : in std_logic_vector (5 downto 0);
   data_out_g: out std_logic_vector(15 downto 0);
   ovrflw: out std_logic 
@@ -77,7 +77,7 @@ begin
 				
 			g		<= 	to_integer(gain_s);
 	
-			data_out_g <= data_in((58-g) downto (58-g-15)); 
+			data_out_g <= data_in((57-g) downto (57-g-15)); 
 	
 	end process;
   
@@ -86,43 +86,43 @@ begin
   
 		begin
   
-			if (data_in(58) = '0') then
+			if (data_in(57) = '0') then
 			
 				case g is
 			
 					when 0 => overflow <= '0';
-					when 1 => overflow <= reduct_or(data_in(58 downto 57));
-					when 2 => overflow <= reduct_or(data_in(58 downto 56));
-					when 3 => overflow <= reduct_or(data_in(58 downto 55));
-					when 4 => overflow <= reduct_or(data_in(58 downto 54));
-					when 5 => overflow <= reduct_or(data_in(58 downto 53));
-					when 6 => overflow <= reduct_or(data_in(58 downto 52));
-					when 7 => overflow <= reduct_or(data_in(58 downto 51));
-					when 8 => overflow <= reduct_or(data_in(58 downto 50));
-					when 9 => overflow <= reduct_or(data_in(58 downto 49));
-					when 10 => overflow <= reduct_or(data_in(58 downto 48));
-					when 11 => overflow <= reduct_or(data_in(58 downto 47));
-					when 12 => overflow <= reduct_or(data_in(58 downto 46));
-					when 13 => overflow <= reduct_or(data_in(58 downto 45));
-					when 14 => overflow <= reduct_or(data_in(58 downto 44));
-					when 15 => overflow <= reduct_or(data_in(58 downto 43));
-					when 16 => overflow <= reduct_or(data_in(58 downto 42));
-					when 17 => overflow <= reduct_or(data_in(58 downto 41));
-					when 18 => overflow <= reduct_or(data_in(58 downto 40));
-					when 19 => overflow <= reduct_or(data_in(58 downto 39));
-					when 20 => overflow <= reduct_or(data_in(58 downto 38));
-					when 21 => overflow <= reduct_or(data_in(58 downto 37));
-					when 22 => overflow <= reduct_or(data_in(58 downto 36));
-					when 23 => overflow <= reduct_or(data_in(58 downto 35));
-					when 24 => overflow <= reduct_or(data_in(58 downto 34));
-					when 25 => overflow <= reduct_or(data_in(58 downto 33));
-					when 26 => overflow <= reduct_or(data_in(58 downto 32));
-					when 27 => overflow <= reduct_or(data_in(58 downto 31));
-					when 28 => overflow <= reduct_or(data_in(58 downto 30));
-					when 29 => overflow <= reduct_or(data_in(58 downto 29));
-					when 30 => overflow <= reduct_or(data_in(58 downto 28));
-					when 31 => overflow <= reduct_or(data_in(58 downto 27));
-					when 32 => overflow <= reduct_or(data_in(58 downto 26));
+					when 1 => overflow <= reduct_or(data_in(57 downto 56));
+					when 2 => overflow <= reduct_or(data_in(57 downto 55));
+					when 3 => overflow <= reduct_or(data_in(57 downto 54));
+					when 4 => overflow <= reduct_or(data_in(57 downto 53));
+					when 5 => overflow <= reduct_or(data_in(57 downto 52));
+					when 6 => overflow <= reduct_or(data_in(57 downto 51));
+					when 7 => overflow <= reduct_or(data_in(57 downto 50));
+					when 8 => overflow <= reduct_or(data_in(57 downto 49));
+					when 9 => overflow <= reduct_or(data_in(57 downto 48));
+					when 10 => overflow <= reduct_or(data_in(57 downto 47));
+					when 11 => overflow <= reduct_or(data_in(57 downto 46));
+					when 12 => overflow <= reduct_or(data_in(57 downto 45));
+					when 13 => overflow <= reduct_or(data_in(57 downto 44));
+					when 14 => overflow <= reduct_or(data_in(57 downto 43));
+					when 15 => overflow <= reduct_or(data_in(57 downto 42));
+					when 16 => overflow <= reduct_or(data_in(57 downto 41));
+					when 17 => overflow <= reduct_or(data_in(57 downto 40));
+					when 18 => overflow <= reduct_or(data_in(57 downto 39));
+					when 19 => overflow <= reduct_or(data_in(57 downto 38));
+					when 20 => overflow <= reduct_or(data_in(57 downto 37));
+					when 21 => overflow <= reduct_or(data_in(57 downto 36));
+					when 22 => overflow <= reduct_or(data_in(57 downto 35));
+					when 23 => overflow <= reduct_or(data_in(57 downto 34));
+					when 24 => overflow <= reduct_or(data_in(57 downto 33));
+					when 25 => overflow <= reduct_or(data_in(57 downto 32));
+					when 26 => overflow <= reduct_or(data_in(57 downto 31));
+					when 27 => overflow <= reduct_or(data_in(57 downto 30));
+					when 28 => overflow <= reduct_or(data_in(57 downto 29));
+					when 29 => overflow <= reduct_or(data_in(57 downto 28));
+					when 30 => overflow <= reduct_or(data_in(57 downto 27));
+					when 31 => overflow <= reduct_or(data_in(57 downto 26));
+					when 32 => overflow <= reduct_or(data_in(57 downto 25));
 
 				end case;
 			
@@ -133,38 +133,38 @@ begin
 				case g is
 			
 					when 0 => overflow <= '0';
-					when 1 => overflow <= reduct_nand(data_in(58 downto 57));
-					when 2 => overflow <= reduct_nand(data_in(58 downto 56));
-					when 3 => overflow <= reduct_nand(data_in(58 downto 55));
-					when 4 => overflow <= reduct_nand(data_in(58 downto 54));
-					when 5 => overflow <= reduct_nand(data_in(58 downto 53));
-					when 6 => overflow <= reduct_nand(data_in(58 downto 52));
-					when 7 => overflow <= reduct_nand(data_in(58 downto 51));
-					when 8 => overflow <= reduct_nand(data_in(58 downto 50));
-					when 9 => overflow <= reduct_nand(data_in(58 downto 49));
-					when 10 => overflow <= reduct_nand(data_in(58 downto 48));
-					when 11 => overflow <= reduct_nand(data_in(58 downto 47));
-					when 12 => overflow <= reduct_nand(data_in(58 downto 46));
-					when 13 => overflow <= reduct_nand(data_in(58 downto 45));
-					when 14 => overflow <= reduct_nand(data_in(58 downto 44));
-					when 15 => overflow <= reduct_nand(data_in(58 downto 43));
-					when 16 => overflow <= reduct_nand(data_in(58 downto 42));
-					when 17 => overflow <= reduct_nand(data_in(58 downto 41));
-					when 18 => overflow <= reduct_nand(data_in(58 downto 40));
-					when 19 => overflow <= reduct_nand(data_in(58 downto 39));
-					when 20 => overflow <= reduct_nand(data_in(58 downto 38));
-					when 21 => overflow <= reduct_nand(data_in(58 downto 37));
-					when 22 => overflow <= reduct_nand(data_in(58 downto 36));
-					when 23 => overflow <= reduct_nand(data_in(58 downto 35));
-					when 24 => overflow <= reduct_nand(data_in(58 downto 34));
-					when 25 => overflow <= reduct_nand(data_in(58 downto 33));
-					when 26 => overflow <= reduct_nand(data_in(58 downto 32));
-					when 27 => overflow <= reduct_nand(data_in(58 downto 31));
-					when 28 => overflow <= reduct_nand(data_in(58 downto 30));
-					when 29 => overflow <= reduct_nand(data_in(58 downto 29));
-					when 30 => overflow <= reduct_nand(data_in(58 downto 28));
-					when 31 => overflow <= reduct_nand(data_in(58 downto 27));
-					when 32 => overflow <= reduct_nand(data_in(58 downto 26));
+					when 1 => overflow <= reduct_nand(data_in(57 downto 56));
+					when 2 => overflow <= reduct_nand(data_in(57 downto 55));
+					when 3 => overflow <= reduct_nand(data_in(57 downto 54));
+					when 4 => overflow <= reduct_nand(data_in(57 downto 53));
+					when 5 => overflow <= reduct_nand(data_in(57 downto 52));
+					when 6 => overflow <= reduct_nand(data_in(57 downto 51));
+					when 7 => overflow <= reduct_nand(data_in(57 downto 50));
+					when 8 => overflow <= reduct_nand(data_in(57 downto 49));
+					when 9 => overflow <= reduct_nand(data_in(57 downto 48));
+					when 10 => overflow <= reduct_nand(data_in(57 downto 47));
+					when 11 => overflow <= reduct_nand(data_in(57 downto 46));
+					when 12 => overflow <= reduct_nand(data_in(57 downto 45));
+					when 13 => overflow <= reduct_nand(data_in(57 downto 44));
+					when 14 => overflow <= reduct_nand(data_in(57 downto 43));
+					when 15 => overflow <= reduct_nand(data_in(57 downto 42));
+					when 16 => overflow <= reduct_nand(data_in(57 downto 41));
+					when 17 => overflow <= reduct_nand(data_in(57 downto 40));
+					when 18 => overflow <= reduct_nand(data_in(57 downto 39));
+					when 19 => overflow <= reduct_nand(data_in(57 downto 38));
+					when 20 => overflow <= reduct_nand(data_in(57 downto 37));
+					when 21 => overflow <= reduct_nand(data_in(57 downto 36));
+					when 22 => overflow <= reduct_nand(data_in(57 downto 35));
+					when 23 => overflow <= reduct_nand(data_in(57 downto 34));
+					when 24 => overflow <= reduct_nand(data_in(57 downto 33));
+					when 25 => overflow <= reduct_nand(data_in(57 downto 32));
+					when 26 => overflow <= reduct_nand(data_in(57 downto 31));
+					when 27 => overflow <= reduct_nand(data_in(57 downto 30));
+					when 28 => overflow <= reduct_nand(data_in(57 downto 29));
+					when 29 => overflow <= reduct_nand(data_in(57 downto 28));
+					when 30 => overflow <= reduct_nand(data_in(57 downto 27));
+					when 31 => overflow <= reduct_nand(data_in(57 downto 26));
+					when 32 => overflow <= reduct_nand(data_in(57 downto 25));
 				
 				end case;
 			
