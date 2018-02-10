8173632293245aa5e801c627f5fa9b3de0ac042d
strongsaurabh <saurabhg@colostate.edu>
2018-02-09 18:14:26 -0700

    fixed math for signed number conversion

--- EightLockins/qsys_system/synthesis/submodules/qsys_system_control_bits.v
+++ EightLockins/qsys_system/synthesis/submodules/qsys_system_control_bits.v
@@ -18,41 +18,49 @@
 // altera message_level Level1 
 // altera message_off 10034 10035 10036 10037 10230 10240 10030 
 
-module qsys_system_lia_1_x (
-                             // inputs:
-                              address,
-                              clk,
-                              in_port,
-                              reset_n,
+module qsys_system_control_bits (
+                                  // inputs:
+                                   address,
+                                   chipselect,
+                                   clk,
+                                   reset_n,
+                                   write_n,
+                                   writedata,
 
-                             // outputs:
-                              readdata
-                           )
+                                  // outputs:
+                                   out_port,
+                                   readdata
+                                )
 ;
 
+  output  [  7: 0] out_port;
   output  [ 31: 0] readdata;
   input   [  1: 0] address;
+  input            chipselect;
   input            clk;
-  input   [ 15: 0] in_port;
   input            reset_n;
+  input            write_n;
+  input   [ 31: 0] writedata;
 
   wire             clk_en;
-  wire    [ 15: 0] data_in;
-  wire    [ 15: 0] read_mux_out;
-  reg     [ 31: 0] readdata;
+  reg     [  7: 0] data_out;
+  wire    [  7: 0] out_port;
+  wire    [  7: 0] read_mux_out;
+  wire    [ 31: 0] readdata;
   assign clk_en = 1;
   //s1, which is an e_avalon_slave
-  assign read_mux_out = {16 {(address == 0)}} & data_in;
+  assign read_mux_out = {8 {(address == 0)}} & data_out;
   always @(posedge clk or negedge reset_n)
     begin
       if (reset_n == 0)
-          readdata <= 0;
-      else if (clk_en)
-          readdata <= {32'b0 | read_mux_out};
+          data_out <= 0;
+      else if (chipselect && ~write_n && (address == 0))
+          data_out <= writedata[7 : 0];
     end
 
 
-  assign data_in = in_port;
+  assign readdata = {32'b0 | read_mux_out};
+  assign out_port = data_out;
 
 endmodule
 
