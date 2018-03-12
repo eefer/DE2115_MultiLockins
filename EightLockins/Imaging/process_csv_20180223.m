7116964e9b407de961960831a3774b46b5d27aac
strongsaurabh <saurabhg@colostate.edu>
2018-03-12 14:42:29 -0600

    minor changes made to the visualization script

--- EightLockins/Imaging/process_csv_20180223.m
+++ EightLockins/Imaging/process_csv_20180223.m
@@ -12,25 +12,34 @@
 %%///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 clc; clear all; close all;
-data = csvread('C:\Users\saurabhg\Desktop\temp\y3.csv',1,3); % skips header
 
+% TODO: MOVE SETTINGS FOR ALL FILE-DEPENDENT PARAMETERS HERE
+
+filename = 'T:\projects\WilsonGroup\8 lockins\data\NIST Data 2018_03_05\NIST Data\x5.csv';
+data = csvread(filename,1,3); % skips header
+%
+aspectRatio = 2;
+
+%%
 for i = 1:16
     xy{i} = data(:,i);
 end
 
-%% make for loop, apply to all X and Y images
+% make for loop, apply to all X and Y images
 % ii= 1;
 for ii = 1:16
 % should be 256x256 after accounting for overscan
-nx = 333;          % samples per line
+
 % data is a list of samples, scan across x first
 % we expect a few dummy rows at the top when done
-   startDelay = 68;
-    
+   
+    nx = 1997;          % samples per line = (FPGA sample rate) / (AFM line rate)
     ny = floor(length(xy{ii}) / nx);
+    startDelay = 0;
     x1_corrected = xy{ii}(1:(nx*ny)); % remove extra pixels if nx*ny < nsamp
     x1_corrected = circshift(x1_corrected,startDelay);
     XY{ii} = reshape(x1_corrected,[nx,ny]).';    
+    XY{ii} = fliplr(XY{ii});
 %     imagesc(XY{ii});
 %     drawnow;
 
@@ -45,14 +54,13 @@ XY{ii} = imtransform(XY{ii}, tform);
 % grid on
 
 % handle flyback / reverse every other row
-nx_final = 138;
+nx_final = 500;     % valid number of samples per line, in a forward scan (should be nx/2*0.8)
 x0_forward = 52;
 % x0_backward = 174;
 X1_forward = XY{ii}(:, x0_forward:(x0_forward+nx_final-1));
-% subplot(121);
 % imagesc(X1_forward)
-% title('forward scan lines')
-% axis image
+% axis image;
+% grid on;
 
 % X1_backward = XY{ii}(:, x0_backward:(x0_backward+nx_final-1));
 % X1_backward = fliplr( X1_backward );
@@ -92,8 +100,11 @@ nf = numel(XY)/2;
 for i = 1:nf
    for j = 1:ny
       for k= 1:nx
-         lockin_amp{i}(j,k)  = sqrt (((XY{2*i-1}(j,k))^2)+((XY{2*i}(j,k))^2));     
-         lockin_phase{i}(j,k)  = atan (XY{2*i}(j,k)/ XY{2*i-1}(j,k));   
+         lockin_amp{i}(j,k)  = sqrt (((XY{2*i-1}(j,k))^2)+((XY{2*i}(j,k))^2));  
+         
+         % phase = atan( Y / X ) 
+         lockin_phase{i}(j,k)  = atan2 (XY{2*i}(j,k), XY{2*i-1}(j,k));   
+         
       end
    end
 end
@@ -111,17 +122,17 @@ h = imagesc(lockin_amp{i});
 axis image;
 set(h,'Tag','ampImage');
 colorbar;
-set(gca,'DataAspectRatio',[0.5,1,1]);
+set(gca,'DataAspectRatio',[aspectRatio,1,1]);
 caxis([0,max(max ([lockin_amp{:}]))]);
-hrect = imrect(gca,[20,40,20,40]);
+hrect = imrect(gca,[20,40,1,1]);
 
 subplot( 2,2,2 );
 h_phase = imagesc(lockin_phase{i});
 set(h_phase,'Tag','phaseImage');
 axis image;
 colorbar;
-set(gca,'DataAspectRatio',[0.5,1,1]);
-caxis([-pi,pi]);
+set(gca,'DataAspectRatio',[aspectRatio,1,1]);
+caxis([-10,10]);
 
 subplot(2,2,(3:4));
 [hAx,hLine1,hLine2] = plotyy(1:nf,squeeze(sum(sum(lockin_amp_mat,1),2)),1:nf,squeeze(sum(sum(lockin_phase_mat,1),2)));
@@ -133,7 +144,7 @@ xlabel('Lockin channel');
 legend('Amplitude','Phase');
 ylabel(hAx(1),'Amplitude'); % left y-axis
 ylabel(hAx(2),'Phase'); % right y-axis
-set(hAx(2),'ylim',[-pi,pi],'ytickmode','auto');
+set(hAx(2),'ylim',[-10,10],'ytickmode','auto');
 set(hAx(1),'ylimmode','auto','ytickmode','auto');
 
 % store data in the figure
