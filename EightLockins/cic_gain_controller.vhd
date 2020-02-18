e1a8adfa57689796b1d786504648d4ac2ee0f715
Jesse Wilson <jesse.wilson@colostate.edu>
2020-02-18 06:46:54 -0700

    documented CIC gain controller

--- EightLockins/cic_gain_controller.vhd
+++ EightLockins/cic_gain_controller.vhd
@@ -1,6 +1,11 @@
---		Gain controller module for cic_filter: selects which 16 bits of the 44 bits of cic filter output is to be passed to the lia_core.vhd
+-- Gain controller module for cic_filter: 
+-- bit slice selection
+-- selects which 16 bits of the 59 bits of cic filter output is to be passed to the lia_core.vhd
+-- Sliding the window to the MSB by one position decreases gain by 2x.
+-- Sliding the window to the LSB by one position increases gain by 2x.
 --		Written by Arya Chowdhury Mugdha (arya.mugdha@colostate.edu)
-
+--
+-- (2020-02-18 JWW) Added comments / documentation
 
 
 LIBRARY ieee;
@@ -10,11 +15,11 @@ USE ieee.numeric_std.all;
 
 entity cic_gain_controller is
 port(
-  clk_in			: in  std_logic;		
-  data_in		: in std_logic_vector(58 downto 0);
-  gain     		: in std_logic_vector (5 downto 0);
-  data_out_g	: out std_logic_vector(15 downto 0);
-  ovrflw			: out std_logic 
+  clk_in		: in  std_logic;						-- input clock
+  data_in		: in std_logic_vector(58 downto 0);		-- 59-bit input
+  gain     		: in std_logic_vector (5 downto 0);		-- gain factor (position of 16-bit slice)
+  data_out_g	: out std_logic_vector(15 downto 0);	-- 16-bit output
+  ovrflw		: out std_logic 						-- indicate overflow (gain too high)
   );
 end cic_gain_controller;
 
@@ -25,78 +30,73 @@ signal g  			: integer range 0 to 32;
 signal result		: std_logic;  	 
 signal overflow	: std_logic;
 
-  
-  
+  -- reducing OR operator
+  -- produces a logical OR of all the bits in a vector
+  -- e.g. reduct_or('001010011') = '1'
+  --      reduct_or('00000') = '0'
+  -- 	  reduct_or('0100000000000') = '1'
+  --	  reduct_or('1111') = '1'
   function reduct_or (inp : in std_logic_vector) return std_logic is
-		
 		variable result : std_logic := '0';
 		
 	begin
-	
 		for i in inp'range loop
-		
 			result := result or inp(i);
-			
 			exit when result = '1';
-			
 		end loop;
-		
 		return result;
-		
 	end function;
 	
-	
+	-- reducing NAND operator
+	-- produces a logical NAND of all the bits in a vector
+	-- e.g. reduct_nand('001010011') = '1'
+	--      reduct_nand('00000') = '1'
+	-- 	    reduct_nand('0100000000000') = '1'
+	--		reduct_nand('1111') = '0'
 	function reduct_nand (inp : in std_logic_vector) return std_logic is
-		
 		variable result : std_logic := '0';
 		
-		
 	begin
-	
 		for i in inp'range loop
-		
 			result := '1' nand inp(i);
-			
 			exit when result = '1';
-			
 		end loop;
-			
 		return result;
-		
 	end function;
-
-
 
 begin
 
 	-- programmable gain process
 	process(clk_in) is
-  
 		begin
-		
+	
 		if rising_edge( clk_in ) then
 			-- convert gain factor to an integer
-			gain_s		<=		signed(gain);
-			g				<= 	to_integer(gain_s);
+			gain_s		<=	signed(gain);
+			g			<= 	to_integer(gain_s);
 			
 			-- perform 2^g gain by selecting a bit slice from data_in
 			data_out_g 	<= data_in((58-g) downto (58-g-15)); 
-			
 		end if;
-	
 	end process;
   
 	-- overflow calculation process
+	-- Bit slice selection discards MSBs and LSBs.
+	-- If the discarded MSBs are all the same as the MSB of the 16-bit slice,
+	-- then they were sign bits, and it is safe to discard them.
+	-- But if any of the discarded MSBs are different from the MSB of the 16-bit slice,
+	-- then the number was out of range for the selected slice,
+	-- and an overflow has occurred.
 	process (clk_in) is -- modified JWW 20180127
-  
 		begin
 		
 		if rising_edge( clk_in ) then
   
 			if (data_in(58) = '0') then
-			
+				-- if sign is positive, examine discarded MSBs for any '1's
+				-- by using a reducing OR operator
 				case g is
-			
+					-- which bits were discarded depends on the gain factor
 					when 0 => overflow <= '0';
 					when 1 => overflow <= reduct_or(data_in(58 downto 57));
 					when 2 => overflow <= reduct_or(data_in(58 downto 56));
@@ -136,9 +136,10 @@ begin
 			ovrflw <= overflow;
 			
 			else
-		
+				-- if sign is negative, examine discarded MSBs for any '0's
+				-- by using a reducing NAND operator
 				case g is
-			
+					-- which bits were discarded depends on the gain factor
 					when 0 => overflow <= '0';
 					when 1 => overflow <= reduct_nand(data_in(58 downto 57));
 					when 2 => overflow <= reduct_nand(data_in(58 downto 56));
